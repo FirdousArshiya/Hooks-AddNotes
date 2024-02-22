cc51f81d7511dd4c155efe73d9daf0ad88efcb36
FirdousArshiya <firdousarshiya99@gmail.com>
2024-02-22 02:19:28 +0000

    notes code

---
+++ src/components/Notes/index.js
@@ -0,0 +1,88 @@
+// Write your code here
+
+import {useState} from 'react'
+import {v4 as uuidv4} from 'uuid'
+import NoteItem from '../NoteItem'
+
+import {
+  MainContainer,
+  NotesContainer,
+  Heading,
+  Form,
+  TitleInput,
+  NoteTextArea,
+  AddButton,
+  EmptyNotesViewContainer,
+  Image,
+  EmptyNotesHeading,
+  Description,
+  NotesList,
+} from './styledComponents'
+
+const Notes = () => {
+  const [title, setTitle] = useState('')
+  const [noteText, setNoteText] = useState('')
+  const [notesList, setNotesList] = useState([])
+
+  const onChangeTitle = event => {
+    setTitle(event.target.value)
+  }
+
+  const onChangeNoteText = event => {
+    setNoteText(event.target.value)
+  }
+
+  const onAddNote = event => {
+    event.preventDefault()
+    const newNoteDetails = {
+      id: uuidv4(),
+      title,
+      noteText,
+    }
+    setNotesList(prevNoteDetails => [...prevNoteDetails, newNoteDetails])
+    setTitle('')
+    setNoteText('')
+  }
+
+  return (
+    <MainContainer>
+      <NotesContainer>
+        <Heading>Notes</Heading>
+        <Form onSubmit={onAddNote}>
+          <TitleInput
+            type="text"
+            placeholder="Title"
+            onChange={onChangeTitle}
+            value={title}
+          />
+          <NoteTextArea
+            placeholder="Take a Note..."
+            value={noteText}
+            onChange={onChangeNoteText}
+            rows="3"
+          />
+          <AddButton type="submit">Add</AddButton>
+        </Form>
+        {notesList.length > 0 ? (
+          <NotesList>
+            {notesList.map(eachNote => (
+              <NoteItem key={eachNote.id} noteDetails={eachNote} />
+            ))}
+          </NotesList>
+        ) : (
+          <>
+            <EmptyNotesViewContainer>
+              <Image
+                src="https://assets.ccbp.in/frontend/hooks/empty-notes-img.png"
+                alt="notes empty"
+              />
+              <EmptyNotesHeading>No Notes Yet</EmptyNotesHeading>
+              <Description>Notes you add will appear here</Description>
+            </EmptyNotesViewContainer>
+          </>
+        )}
+      </NotesContainer>
+    </MainContainer>
+  )
+}
+export default Notes
